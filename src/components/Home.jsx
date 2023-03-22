184bf1bc975d7d17c4b599a9eb73a3369a1f12d1
Abhishek Bahukhandi <93070874+abrknine@users.noreply.github.com>
2023-03-23 01:46:25 +0530

    bitc

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,9 @@ import './Home.css'
 
 const Home = () => {
     return (
-        <div className="home mx-12 my-44 ">
+        <div className="home  ">
           <div className="content my-24  hom ">
-            <h1 className=' hom1'>You're not alone. <br /> We're here for you.</h1>
+            <h1 className='font-semibold hom1'>You're not alone. <br /> We're here for you.</h1>
             <p className='mx-12 hom2'> Our platform encourages open conversations about mental health, without judgment or shame. Share your experiences and help others do the same.
                      </p>
                      <div className='flex justify-evenly hom3 '>
